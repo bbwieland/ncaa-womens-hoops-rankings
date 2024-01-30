3f13283c06f9e2102c42b017400cf20a32c9b70a
bbwieland <benbowenwieland@gmail.com>
2024-01-30 10:20:43 -0500

    Automated script

---
+++ commit_and_push_csv_to_git.R
@@ -0,0 +1,15 @@
+library(git2r)
+
+source("wbb_adjem_script.R", echo = FALSE)
+
+repo_path <- "/Users/ben/Desktop/Code/wbb-rankings"
+current_date_time <- format(Sys.time(), "%b %d, %Y at %H:%M %p %Z")
+
+git2r::add(repo = repo_path,
+           path = "landing_page.csv")
+
+git2r::commit(repo = repo_path,
+              message = paste0("Data update: ", current_date_time))
+
+git2r::push(object = repo_path,
+            credentials = cred_token())
